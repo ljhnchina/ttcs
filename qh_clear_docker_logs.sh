bea3b62f4f2d7c6f10ca828447a805d6bde7c8fe
ljhnchina <jiandjh@qq.com>
2023-02-27 20:19:25 +0800

    群晖的删除docker log脚本

--- qh_clear_docker_logs.sh
+++ qh_clear_docker_logs.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-
+Author: Squaregentleman
+Modifier: Oreo
+Date: Tue Aug 10 08:24:30 UTC 2023
+cron: 30 23 * * *
+new Env('删除docker log');
 echo "======== start clean docker containers logs ========"
 
 logs=$(find /volume1/@docker/containers/ -name log.db)
